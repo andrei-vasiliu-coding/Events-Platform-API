7b24a9539584b543a3f14d01ada9ec2a1fc882a0
Andrei Vasiliu <andrei-vasiliu@outlook.com>
2025-03-03 13:06:12 +0000

    feat: implement OrganiserService methods in OrganiserServiceImpl

--- Java_Events_Platform/Java_Events_Platform/src/main/java/com/jveventsplatform/Java_Events_Platform/service/OrganiserService.java
+++ Java_Events_Platform/Java_Events_Platform/src/main/java/com/jveventsplatform/Java_Events_Platform/service/OrganiserService.java
@@ -1,4 +1,11 @@
 package com.jveventsplatform.Java_Events_Platform.service;
 
+import com.jveventsplatform.Java_Events_Platform.model.Organiser;
+
+import java.util.List;
+
 public interface OrganiserService {
+    List<Organiser> getOrganiserByName(String name);
+    List<Organiser> getOrganiserByEmail(String email);
+    List<Organiser> getOrganiserByPhoneNumber(String phoneNumber);
 }
--- Java_Events_Platform/Java_Events_Platform/src/main/java/com/jveventsplatform/Java_Events_Platform/service/OrganiserServiceImpl.java
+++ Java_Events_Platform/Java_Events_Platform/src/main/java/com/jveventsplatform/Java_Events_Platform/service/OrganiserServiceImpl.java
@@ -1,4 +1,28 @@
 package com.jveventsplatform.Java_Events_Platform.service;
 
-public class OrganiserServiceImpl {
+import com.jveventsplatform.Java_Events_Platform.model.Organiser;
+import com.jveventsplatform.Java_Events_Platform.repository.OrganiserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+public class OrganiserServiceImpl implements OrganiserService {
+
+    @Autowired
+    OrganiserRepository organiserRepository;
+
+    @Override
+    public List<Organiser> getOrganiserByName(String name) {
+        return organiserRepository.findByName(name);
+    }
+
+    @Override
+    public List<Organiser> getOrganiserByEmail(String email) {
+        return organiserRepository.findByEmail(email);
+    }
+
+    @Override
+    public List<Organiser> getOrganiserByPhoneNumber(String phoneNumber) {
+        return organiserRepository.findByPhoneNumber(phoneNumber);
+    }
 }
